e94d0ccba149649f2a32be6008490974644ef7f9
codelearner777 <mulyantobudi26@yahoo.com>
2015-11-06 12:49:18 +0700

    postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151103075731) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.string   "text"
@@ -28,7 +31,7 @@ ActiveRecord::Schema.define(version: 20151103075731) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -39,4 +42,5 @@ ActiveRecord::Schema.define(version: 20151103075731) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
